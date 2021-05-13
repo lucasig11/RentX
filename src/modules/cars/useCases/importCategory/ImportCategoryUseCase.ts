7265e64380dd72cfdf6ee15ca99853513d52b087
lucas <lucasfonsecard@icloud.com>
2021-05-12 22:30:46 -0300

    fix: delete tmp file after download

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -27,6 +27,7 @@ export default class ImportCategoryUseCase {
                     });
                 })
                 .on('end', () => {
+                    fs.promises.unlink(file.path);
                     resolve(categories);
                 })
                 .on('error', (err) => {
