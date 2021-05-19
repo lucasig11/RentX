375339e2befa7ebf0a54ebfa8e5e8e7c2a7e7676
lucas <lucasfonsecard@icloud.com>
2021-05-19 16:21:13 -0300

    register specification repository singletion

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -2,6 +2,6 @@ import Specification from '../entities/Specification';
 import { ICreateSpecificationDTO } from './dtos/ICreateSpecificationDTO';
 
 export default interface ISpecificationsRepository {
-    create(data: ICreateSpecificationDTO): void;
-    findByName(name: string): Specification;
+    create(data: ICreateSpecificationDTO): Promise<void>;
+    findByName(name: string): Promise<Specification>;
 }
---
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -0,0 +1,35 @@
+import { getRepository, Repository } from 'typeorm';
+
+import Specification from '../../entities/Specification';
+import { ICreateSpecificationDTO } from '../dtos/ICreateSpecificationDTO';
+import ISpecificationsRepository from '../ISpecificationsRepository';
+
+export default class SpecificationsRepository
+    implements ISpecificationsRepository
+{
+    private repository: Repository<Specification>;
+
+    constructor() {
+        this.repository = getRepository(Specification);
+    }
+
+    public async findByName(name: string): Promise<Specification> {
+        const specification = this.repository.findOne({
+            where: { name },
+        });
+
+        return specification;
+    }
+
+    public async create({
+        name,
+        description,
+    }: ICreateSpecificationDTO): Promise<void> {
+        const specification = this.repository.create({
+            name,
+            description,
+        });
+
+        await this.repository.save(specification);
+    }
+}
