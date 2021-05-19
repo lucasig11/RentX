886f13a927a1b17d44d17742c3d0d2f2f92e228f
lucas <lucasfonsecard@icloud.com>
2021-05-19 15:35:17 -0300

    create categories repository container

---
+++ src/shared/container/index.ts
@@ -0,0 +1,9 @@
+import CategoriesRepository from 'modules/cars/repositories/implementations/CategoriesRepository';
+import { container } from 'tsyringe';
+
+import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
+
+container.registerSingleton<ICategoriesRepository>(
+    'CategoriesRepository',
+    CategoriesRepository
+);
