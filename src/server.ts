ee267d737a6436ae4e91640454d2834e3a7f103b
lucas <lucasfonsecard@icloud.com>
2021-05-19 16:38:50 -0300

    add AppError and error fallback

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
-import express from 'express';
+import 'reflect-metadata';
+import 'express-async-errors';
+import express, { Request, Response, NextFunction } from 'express';
+import AppError from 'shared/errors/AppError';
 import swaggerUI from 'swagger-ui-express';
 
 import './database';
 import './shared/container';
+
 import routes from './routes';
 import swaggerFile from './swagger.json';
 
@@ -11,4 +15,19 @@ const app = express();
 app.use(express.json());
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use(routes);
+app.use(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+        if (err instanceof AppError) {
+            return response.status(err.statusCode).json({
+                status: 'error',
+                message: err.message,
+            });
+        }
+        return response.status(500).json({
+            status: 'error',
+            message: 'Internal server error.',
+        });
+    }
+);
 app.listen(3333, () => console.log('Server started succesfully'));
