73bcaaace34a2756d18dc1dac2646d7357438b7d
lucas <lucasfonsecard@icloud.com>
2021-05-20 01:50:58 -0300

    add verify method to token provider

--- src/modules/accounts/providers/TokenProvider/implementations/JWTTokenProvider.ts
+++ src/modules/accounts/providers/TokenProvider/implementations/JWTTokenProvider.ts
@@ -1,12 +1,36 @@
 import authConfig from '@config/auth';
-import { sign } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
+
+import AppError from '@shared/errors/AppError';
 
 import ITokenProvider from '../models/ITokenProvider';
 
-export default class JWTTokenProvider implements ITokenProvider {
-    generate(user_id: string): string {
-        const { secret, expiresIn } = authConfig.jwt;
+interface IJWTToken {
+    iat: number;
+    exp: number;
+    sub: string;
+}
 
+export default class JWTTokenProvider implements ITokenProvider {
+    private config: typeof authConfig.jwt;
+
+    constructor() {
+        this.config = authConfig.jwt;
+    }
+
+    async verify(token: string): Promise<string> {
+        const { secret } = this.config;
+
+        try {
+            const { sub } = verify(token, secret) as IJWTToken;
+            return sub;
+        } catch {
+            throw new AppError('Invalid JWT token.', 401);
+        }
+    }
+
+    generate(user_id: string): string {
+        const { secret, expiresIn } = this.config;
         const token = sign({}, secret, {
             subject: user_id,
             expiresIn,
--- src/modules/accounts/providers/TokenProvider/models/ITokenProvider.ts
+++ src/modules/accounts/providers/TokenProvider/models/ITokenProvider.ts
@@ -1,3 +1,4 @@
 export default interface ITokenProvider {
     generate(user_id: string): string;
+    verify(token: string): Promise<string>;
 }
