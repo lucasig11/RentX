34572b68906fecec86613899b173c20ebce02c5f
lucas <lucasfonsecard@icloud.com>
2021-05-19 22:22:15 -0300

    register users repository container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,10 @@
+import UsersRepository from '@modules/accounts/repositories/implementations/UsersRepository';
+import IUsersRepository from '@modules/accounts/repositories/IUsersRepository';
+import ICategoriesRepository from '@modules/cars/repositories/ICategoriesRepository';
+import CategoriesRepository from '@modules/cars/repositories/implementations/CategoriesRepository';
+import SpecificationsRepository from '@modules/cars/repositories/implementations/SpecificationsRepository';
+import ISpecificationsRepository from '@modules/cars/repositories/ISpecificationsRepository';
 import { container } from 'tsyringe';
-
-import ICategoriesRepository from '../../modules/cars/repositories/ICategoriesRepository';
-import CategoriesRepository from '../../modules/cars/repositories/implementations/CategoriesRepository';
-import SpecificationsRepository from '../../modules/cars/repositories/implementations/SpecificationsRepository';
-import ISpecificationsRepository from '../../modules/cars/repositories/ISpecificationsRepository';
 
 container.registerSingleton<ICategoriesRepository>(
     'CategoriesRepository',
@@ -13,4 +14,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
     'SpecificationsRepository',
     SpecificationsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+    'UsersRepository',
+    UsersRepository
 );
