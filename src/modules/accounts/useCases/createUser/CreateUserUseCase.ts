a1e53ad3b86745730b681736105eb4586fd94703
lucas <lucasfonsecard@icloud.com>
2021-05-19 22:49:17 -0300

    add hash provider to create user use case

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { ICreateUserDTO } from '@modules/accounts/dtos/ICreateUserDTO';
+import IHashProvider from '@modules/accounts/providers/HashProvider/models/IHashProvider';
 import IUsersRepository from '@modules/accounts/repositories/IUsersRepository';
 import { inject, injectable } from 'tsyringe';
 
@@ -6,10 +7,25 @@ import { inject, injectable } from 'tsyringe';
 export class CreateUserUseCase {
     constructor(
         @inject('UsersRepository')
-        private usersRepository: IUsersRepository
+        private usersRepository: IUsersRepository,
+
+        @inject('HashProvider')
+        private hashProvider: IHashProvider
     ) {}
 
-    public async execute(data: ICreateUserDTO): Promise<void> {
-        await this.usersRepository.create(data);
+    public async execute({
+        name,
+        password,
+        email,
+        driver_license,
+    }: ICreateUserDTO): Promise<void> {
+        const passwordHash = await this.hashProvider.generateHash(password);
+
+        await this.usersRepository.create({
+            name,
+            password: passwordHash,
+            email,
+            driver_license,
+        });
     }
 }
