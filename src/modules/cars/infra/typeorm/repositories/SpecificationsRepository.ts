a1b5d98d50c5ec1a92d81174167d1fed203a0d1b
lucas <lucasfonsecard@icloud.com>
2021-05-24 20:25:59 -0300

    update imports

--- src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -1,8 +1,8 @@
+import { ICreateSpecificationDTO } from '@modules/cars/dtos/ICreateSpecificationDTO';
+import ISpecificationsRepository from '@modules/cars/repositories/ISpecificationsRepository';
 import { getRepository, Repository } from 'typeorm';
 
-import { ICreateSpecificationDTO } from '../../dtos/ICreateSpecificationDTO';
-import Specification from '../../entities/Specification';
-import ISpecificationsRepository from '../ISpecificationsRepository';
+import Specification from '../entities/Specification';
 
 export default class SpecificationsRepository
     implements ISpecificationsRepository
