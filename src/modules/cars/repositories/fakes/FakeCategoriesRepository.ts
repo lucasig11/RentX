58b765464d96c431d8abe2dec540ca69e4604950
lucas <lucasfonsecard@icloud.com>
2021-05-24 19:52:03 -0300

    add fake categories repository

---
+++ src/modules/cars/repositories/fakes/FakeCategoriesRepository.ts
@@ -0,0 +1,28 @@
+import ICreateCategoryDTO from '@modules/cars/dtos/ICreateCategoryDTO';
+import Category from '@modules/cars/entities/Category';
+import { v4 } from 'uuid';
+
+import ICategoriesRepository from '../ICategoriesRepository';
+
+export default class FakeCategoriesRepository implements ICategoriesRepository {
+    private repository: Category[] = [];
+
+    public async findByName(name: string): Promise<Category> {
+        const find = this.repository.find((category) => category.name === name);
+
+        return find;
+    }
+
+    public async create(data: ICreateCategoryDTO): Promise<Category> {
+        const category = new Category();
+        Object.assign(category, { id: v4() }, data);
+
+        this.repository.push(category);
+
+        return category;
+    }
+
+    public async index(): Promise<Category[]> {
+        return this.repository;
+    }
+}
