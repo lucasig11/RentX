06b3bb0ed73449855729816cf14ed5230a680441
lucas <lucasfonsecard@icloud.com>
2021-05-24 19:52:48 -0300

    add first test

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,40 @@
+import FakeCategoriesRepository from '@modules/cars/repositories/fakes/FakeCategoriesRepository';
+
+import AppError from '@shared/errors/AppError';
+
+import CreateCategoryUseCase from './CreateCategoryUseCase';
+
+let fakeCategoriesRepository: FakeCategoriesRepository;
+let createCategoryUseCase: CreateCategoryUseCase;
+
+describe('Create new category', () => {
+    beforeEach(() => {
+        fakeCategoriesRepository = new FakeCategoriesRepository();
+        createCategoryUseCase = new CreateCategoryUseCase(
+            fakeCategoriesRepository
+        );
+    });
+
+    it('should be able to create a new category', async () => {
+        const newCategory = await createCategoryUseCase.execute({
+            name: 'TestCategory',
+            description: 'TestCategory',
+        });
+
+        expect(newCategory).toHaveProperty('id');
+    });
+
+    it('should not be able to create two categories w/ the same name', async () => {
+        await createCategoryUseCase.execute({
+            name: 'TestCategory',
+            description: 'TestCategory',
+        });
+
+        await expect(
+            createCategoryUseCase.execute({
+                name: 'TestCategory',
+                description: 'TestCategory',
+            })
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});
