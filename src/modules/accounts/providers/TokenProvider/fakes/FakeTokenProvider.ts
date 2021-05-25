684b61938d835ea2b071e7afda61134389d6c55a
lucas <lucasfonsecard@icloud.com>
2021-05-25 14:41:03 -0300

    implement verify method in fake token provider

--- src/modules/accounts/providers/TokenProvider/fakes/FakeTokenProvider.ts
+++ src/modules/accounts/providers/TokenProvider/fakes/FakeTokenProvider.ts
@@ -1,6 +1,10 @@
 import ITokenProvider from '../models/ITokenProvider';
 
 export default class FakeTokenProvider implements ITokenProvider {
+    async verify(token: string): Promise<string> {
+        return token;
+    }
+
     generate(user_id: string): string {
         return user_id;
     }
