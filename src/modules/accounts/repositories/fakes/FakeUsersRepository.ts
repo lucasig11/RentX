827755446c864bee8771200ecfdd42278d2b6d9b
lucas <lucasfonsecard@icloud.com>
2021-05-25 14:41:10 -0300

    add fake users repository

---
+++ src/modules/accounts/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,32 @@
+import { ICreateUserDTO } from '@modules/accounts/dtos/ICreateUserDTO';
+import { User } from '@modules/accounts/infra/typeorm/entities/User';
+import { v4 } from 'uuid';
+
+import IUsersRepository from '../IUsersRepository';
+
+export default class FakeUsersRepository implements IUsersRepository {
+    private repository: User[] = [];
+
+    public async create(data: ICreateUserDTO): Promise<User> {
+        const user = {
+            ...data,
+            id: v4(),
+            created_at: new Date(),
+            is_admin: false,
+        };
+        this.repository.push(user);
+        return user;
+    }
+
+    public async findByEmail(email: string): Promise<User> {
+        const find = this.repository.find((user) => user.email === email);
+
+        return find;
+    }
+
+    public async findByID(user_id: string): Promise<User> {
+        const find = this.repository.find((user) => user.id === user_id);
+
+        return find;
+    }
+}
