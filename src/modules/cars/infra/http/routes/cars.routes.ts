41dfc6569a1b10f16a10915fb8bfaaead67419e1
lucas <lucasfonsecard@icloud.com>
2021-05-27 08:29:02 -0300

    change 'listCar' directory  to 'listAvailableCars'

--- src/modules/cars/infra/http/routes/cars.routes.ts
+++ src/modules/cars/infra/http/routes/cars.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 import CreateCarController from '@modules/cars/useCases/createCar/CreateCarController';
-import ListAvailableCarsController from '@modules/cars/useCases/listCar/ListAvailableCarsController';
+import ListAvailableCarsController from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
 import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 
