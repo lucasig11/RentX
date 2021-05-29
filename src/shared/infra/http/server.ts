c7d02cab5151ad467a09fd49f3b43e0628691b74
lucas <lucasfonsecard@icloud.com>
2021-05-29 15:47:05 -0300

    refactor server

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,36 +1,3 @@
-import 'reflect-metadata';
-import 'express-async-errors';
+import { app } from './app';
 
-import express, { Request, Response, NextFunction } from 'express';
-import swaggerUI from 'swagger-ui-express';
-
-import AppError from '@shared/errors/AppError';
-
-import swaggerFile from '../../../swagger.json';
-import '@shared/container';
-import createConnection from '../typeorm';
-import routes from './routes';
-
-createConnection();
-const app = express();
-
-app.use(express.json());
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
-app.use(routes);
-app.use(
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    (err: Error, request: Request, response: Response, next: NextFunction) => {
-        if (err instanceof AppError) {
-            return response.status(err.statusCode).json({
-                status: 'error',
-                message: err.message,
-            });
-        }
-        console.log(err);
-        return response.status(500).json({
-            status: 'error',
-            message: `Internal server error - ${err.message}`,
-        });
-    }
-);
 app.listen(3333, () => console.log('Server started succesfully'));
