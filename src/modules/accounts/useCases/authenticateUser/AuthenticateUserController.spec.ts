26226d29463a9461d723d89f64f7cecc4f6c9402
lucas <lucasfonsecard@icloud.com>
2021-05-30 08:26:54 -0300

    add tests for authentication controller

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.spec.ts
@@ -0,0 +1,63 @@
+import 'reflect-metadata';
+import request from 'supertest';
+import { container } from 'tsyringe';
+import { Connection } from 'typeorm';
+import { v4 } from 'uuid';
+
+import IHashProvider from '@modules/accounts/providers/HashProvider/models/IHashProvider';
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+
+import '@shared/container';
+
+let connection: Connection;
+
+describe('Sessions controller', () => {
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
+
+        const hashProvider: IHashProvider = container.resolve('HashProvider');
+
+        const password = await hashProvider.generateHash('password');
+
+        await connection.query(
+            `INSERT INTO USERS(id, name, email, password, is_admin, created_at, driver_license)
+            VALUES('${v4()}', 'regular_user', 'user@email.com', '${password}', false, 'now()', 'xxxxxxx');
+            `
+        );
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+
+    it('should be able to authenticate a user', async () => {
+        const response = await request(app).post('/sessions').send({
+            email: 'user@email.com',
+            password: 'password',
+        });
+
+        expect(response.status).toBe(200);
+        expect(response.body.user.name).toBe('regular_user');
+    });
+
+    it('should not be able to authenticate with incorrect password', async () => {
+        const response = await request(app).post('/sessions').send({
+            email: 'user@email.com',
+            password: 'incorrect_password',
+        });
+
+        expect(response.status).toBe(401);
+    });
+
+    it('should not be able to authenticate with invalid e-mail', async () => {
+        const response = await request(app).post('/sessions').send({
+            email: 'unexisting_user@email.com',
+            password: 'password',
+        });
+
+        expect(response.status).toBe(401);
+    });
+});
