07f12dcbb99920f0eb9c8ae79a85b3eab273c7b0
lucas <lucasfonsecard@icloud.com>
2021-05-30 06:52:09 -0300

    add tests for create specification controller

---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.spec.ts
@@ -0,0 +1,88 @@
+import 'reflect-metadata';
+import request from 'supertest';
+import { container } from 'tsyringe';
+import { Connection } from 'typeorm';
+import { v4 } from 'uuid';
+
+import IHashProvider from '@modules/accounts/providers/HashProvider/models/IHashProvider';
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+
+import '@shared/container';
+
+let connection: Connection;
+
+describe('Create specification controller', () => {
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
+
+        const hashProvider: IHashProvider = container.resolve('HashProvider');
+
+        const password = await hashProvider.generateHash('admin');
+
+        await connection.query(
+            `INSERT INTO USERS(id, name, email, password, is_admin, created_at, driver_license)
+            VALUES('${v4()}', 'admin', 'admin@rentx.com', '${password}', true, 'now()', 'xxxxxxx');
+            INSERT INTO USERS(id, name, email, password, is_admin, created_at, driver_license)
+            VALUES('${v4()}', 'regular_user', 'user@email.com', '${password}', false, 'now()', 'xxxxxxx');
+            `
+        );
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+
+    it('should be able to create a new specification', async () => {
+        const authResponse = await request(app).post('/sessions').send({
+            email: 'admin@rentx.com',
+            password: 'admin',
+        });
+
+        const { token } = authResponse.body;
+
+        const response = await request(app)
+            .post('/specifications')
+            .send({
+                name: 'specification',
+                description: 'specification',
+            })
+            .set({
+                Authorization: `Bearer ${token}`,
+            });
+
+        expect(response.status).toBe(201);
+    });
+
+    it('should throw an error if the user is not authenticated', async () => {
+        const response = await request(app).post('/specifications').send({
+            name: 'specification',
+            description: 'specification',
+        });
+
+        expect(response.status).toBe(401);
+    });
+
+    it('should throw an error if the user is not an admin', async () => {
+        const authResponse = await request(app).post('/sessions').send({
+            email: 'user@email.com',
+            password: 'admin',
+        });
+
+        const { token } = authResponse.body;
+
+        const response = await request(app)
+            .post('/specifications')
+            .send({
+                name: 'specification',
+                description: 'specification',
+            })
+            .set({
+                Authorization: `Bearer ${token}`,
+            });
+
+        expect(response.status).toBe(501);
+    });
+});
