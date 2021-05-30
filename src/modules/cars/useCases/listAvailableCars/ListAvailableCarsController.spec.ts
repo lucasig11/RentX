18ba9fb3407b7354be1afae9ef05290f8802e4bc
lucas <lucasfonsecard@icloud.com>
2021-05-30 08:32:01 -0300

    add tests for ListAvailableCarsController

---
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsController.spec.ts
@@ -0,0 +1,28 @@
+import 'reflect-metadata';
+import request from 'supertest';
+import { Connection } from 'typeorm';
+
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+
+import '@shared/container';
+
+let connection: Connection;
+
+describe('List categories controller', () => {
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+
+    it('should be list all available cars', async () => {
+        const response = await request(app).get('/cars');
+
+        expect(response.status).toBe(200);
+    });
+});
