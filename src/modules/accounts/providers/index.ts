659a85f1190dba2df61fe42d8df00d61d604381a
lucas <lucasfonsecard@icloud.com>
2021-06-01 20:20:29 -0300

    feat: register user tokens repository singleton

--- src/modules/accounts/providers/index.ts
+++ src/modules/accounts/providers/index.ts
@@ -3,6 +3,8 @@ import { container } from 'tsyringe';
 import UsersRepository from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import IUsersRepository from '@modules/accounts/repositories/IUsersRepository';
 
+import UserTokensRepository from '../infra/typeorm/repositories/UserTokensRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 import BCryptHashProvider from './HashProvider/implementations/BCryptHashProvider';
 import IHashProvider from './HashProvider/models/IHashProvider';
 import JWTTokenProvider from './TokenProvider/implementations/JWTTokenProvider';
@@ -10,7 +12,13 @@ import ITokenProvider from './TokenProvider/models/ITokenProvider';
 
 container.registerSingleton<IHashProvider>('HashProvider', BCryptHashProvider);
 container.registerSingleton<ITokenProvider>('TokenProvider', JWTTokenProvider);
+
 container.registerSingleton<IUsersRepository>(
     'UsersRepository',
     UsersRepository
+);
+
+container.registerSingleton<IUserTokensRepository>(
+    'UserTokensRepository',
+    UserTokensRepository
 );
