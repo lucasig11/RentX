5def4a65ee44b14c7ec4da1864cd1a74aa2d205d
lucas <lucasfonsecard@icloud.com>
2021-06-07 19:27:32 -0300

    refactor: register storage provider singleton

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,10 +4,17 @@ import DayJsDateProvider from './DateProvider/implementations/DayJsDateProvider'
 import IDateProvider from './DateProvider/models/IDateProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
+import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
+import IStorageProvider from './StorageProvider/models/IStorageProvider';
 
 container.registerSingleton<IDateProvider>('DateProvider', DayJsDateProvider);
 
 container.registerInstance<IMailProvider>(
     'MailProvider',
     new EtherealMailProvider()
+);
+
+container.registerSingleton<IStorageProvider>(
+    'StorageProvider',
+    DiskStorageProvider
 );
