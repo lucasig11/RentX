5032a5b045b1ec60beabfd33c0f9d2e1e3368a42
lucas <lucasfonsecard@icloud.com>
2021-06-08 16:08:14 -0300

    fix: update build settings

--- babel.config.js
+++ babel.config.js
@@ -16,8 +16,8 @@ module.exports = {
                 }
             }
         ],
-        'babel-plugin-transform-typescript-metadata',
-        ['@babel/plugin-proposal-decorators', { legacy: true }]
+        ['babel-plugin-transform-typescript-metadata'],
+        ['@babel/plugin-proposal-decorators', { legacy: true }],
         ['@babel/plugin-proposal-class-properties', { loose: true }]
     ]
 }
