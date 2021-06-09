6ccaccce892ed5b391a72bc2feab75b2f2f07548
lucas <lucasfonsecard@icloud.com>
2021-06-08 23:41:07 -0300

    ci(sentry): add sentry middlewares

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -8,6 +8,8 @@ import swaggerUI from 'swagger-ui-express';
 
 import uploadConfig from '@config/upload';
 import swaggerFile from '@docs/swagger.json';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 
 import '@shared/container';
 
@@ -22,6 +24,20 @@ const app = express();
 
 app.use(express.json());
 
+app.use(rateLimiter);
+
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(
     '/files/avatar',
     express.static(path.join(uploadConfig.tmpFolder, 'avatar'))
@@ -32,11 +48,11 @@ app.use(
     express.static(path.join(uploadConfig.tmpFolder, 'cars'))
 );
 
-app.use(rateLimiter);
-
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(routes);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(errorFallback);
 
